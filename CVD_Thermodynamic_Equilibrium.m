98f9a5792794869aad2d52b5c4c696088c2846c9
--global <--global>
2020-11-12 14:16:14 -0500

    2nd version

--- CVD_Thermodynamic_Equilibrium.m
+++ CVD_Thermodynamic_Equilibrium.m
@@ -14,30 +14,92 @@ sheet = 'Ex4_3';
 rangeAbund ='A1:H5';
 range_mu ='B9:H9';
 range_mole ='B11:H11';
+
 %abundance Matrix
 [numb,labels,abundance_Matrix] = xlsread(file,sheet,rangeAbund);
 % refference potentials  kJ/mole
-[muReff,txt1,pot] = xlsread(file,sheet,range_mu);
+[muReff] = xlsread(file,sheet,range_mu);
 % intial estimate of moles 
-[intMole,txt2,mole]= xlsread(file,sheet,range_mole);
+% [intMole,txt2,mole]= xlsread(file,sheet,range_mole);
+[intMole] = xlsread(file,sheet,range_mole);
+% rangeb = 'K2:K5';
+% rangeSC =  'B2:H5';
+% AbundanceSC= xlsread(file,sheet,rangeSC);
+% [b] = xlsread(file,sheet,rangeb);
+% no = AbundanceSC\b
+
 %Enter thermodynamics data
 R = 8.314/1000; % kJ/mole K
-P = 40; %atm
-T= 220; %Kelvin
-
+P = input('Enter Pressure in atm :'); %atm
+T= input('Enter Temperature in Kelvin: '); %Kelvin
+iter = input('Enter number of Iterations: ');
+tol  = input('Enter the tolerance: ');
 
 % Calualte stiohcometeric vectors from null space of formula matrix 
-abundance_Matrix
-ZE1 =null(numb,'r')'
+%abundance_Matrix
+FV =null(numb,'r')';
 % test conservation
- numb*ZE1' 
+% numb*FV1' 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Thermodymanics equilibrium  chemical potiental
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+% Newton Raphson minimization method
+ 
+ moles = intMole();
+ nt =sum(moles);
 
+       for s = 1 : length(moles) 
+        mu(s) = muReff(s)+ (R*T*log(moles(s)/nt));
+       end
+       gibbs = mu ./FV;
+ i = 1;      
+     while i <= iter
+        gibbs = gibbs;
+   
+    % claculate jacobian based on moles jac()
+     for k = 1:rank(FV)
+          for j = 1 : length(moles) 
+      % kd = kroneker delta       
+%               if k==j
+%                   kd =1;
+%               else
+%                   kd=0;
+%               end
+               kd =1;
+               jac(k,j) = R*T*(kd/moles(j)-1/nt);
+          end
+     end
+     jacI = 1./(jac' * jac) * jac'; 
+     % claculate extent reaction
 
+     % clac new amount moles 
+     for e =1 : rank(FV)
+       for m =  1:length(moles)
+         moles(m) = moles(m) - w*jacI * extentRx(e);      
+       end
+     end 
+     nt = summ(moles);
+      % create potitial E matrix
+      
+     for s = 1 : length(moles) 
+      mu(s) = muReff(s)+ (R*T*log(moles(s)/nt));
+     end
+     gibbs2 = mu .* FV;
+     deltaGibbs = gibbs1-gibbs;
+       if deltaGibbs < tol
+           break
+       end
+         
+      i = i+1;
+     end
+
+ 
+ 
+%   fprintf('\nApibb = Zprox mu = %11.8f \n\n',mu);     
+%fprintf('\nApprox Gibbs = %11.8f ',gibbsE);     
+  
 
 
 
